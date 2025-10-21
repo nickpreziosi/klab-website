6a1196867bb31c1d75196592d0058dbf178971d7
nicholaspreziosi <108760764+nicholaspreziosi@users.noreply.github.com>
2025-10-21 12:42:41 -0400

    Navbar updates and implement hero section

---
+++ src/app/components/hero/hero.tsx
@@ -0,0 +1,40 @@
+import styles from "./hero.module.css";
+import { AspectRatio } from "radix-ui";
+import "./hero.module.css";
+
+export const Hero = () => {
+  return (
+    <>
+      <video
+        width={720}
+        autoPlay={true}
+        loop={true}
+        muted={true}
+        className={styles.backgroundVideo}
+        src="https://www.pexels.com/download/video/34268861/"
+      ></video>
+      <div className={styles.content}>
+        <div className={styles.mainContainer}>
+          <div className={styles.mainTextContainer}>
+            <h1 className={styles.mainHeading}>We Pay. You Grow.</h1>
+            <p className={styles.mainText}>
+              B2B digital payment and inventory financing solutions that
+              streamline financial transactions between buyers and suppliers.
+            </p>
+          </div>
+        </div>
+        <AspectRatio.Root className={styles.videoEmbedContainer} ratio={16 / 9}>
+          <iframe
+            className={styles.videoEmbed}
+            src="https://www.youtube.com/embed/Ivd6J240bNs?si=pyT7u2GxKWe1Izey"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </AspectRatio.Root>
+      </div>
+    </>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
 import styles from "./page.module.css";
 import { Logo } from "./components/logo/logo";
 import { NavigationMenuDemo } from "./components/navbar/navbar";
+import { Hero } from "./components/hero/hero";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <NavigationMenuDemo></NavigationMenuDemo>
-
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "200px",
-          }}
-        >
-          <Logo size="xl" animated="constant" />
-        </div>
+        <Hero></Hero>
       </main>
     </div>
   );
